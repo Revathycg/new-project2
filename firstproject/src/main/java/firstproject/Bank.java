0d657f8ed05c77280c54850bfd5fe63acda5ef2d
Admin <Admin@DESKTOP-IEPVL3O>
2023-11-07 19:30:20 +0530

    commiting

---
+++ firstproject/src/main/java/firstproject/Bank.java
@@ -0,0 +1,14 @@
+package firstproject;
+
+public class Bank {
+ private int pinnumber; 
+	
+	public int getPinnumber() {
+		
+		return pinnumber;
+	}
+	public void setPinnumber(int pinnumber) {
+		this.pinnumber = pinnumber;
+	}
+
+}
